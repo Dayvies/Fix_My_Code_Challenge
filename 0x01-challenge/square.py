ecf20746c316bcb8b9ad62461a6041c41671cf07
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-10-06 22:36:09 +0300

    square debug

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        if (self.width != self.height) and self.width >= 1:
-                self.height = self.width
-        if (self.width != self.height) and self.height >= 1:
-                self.width = self.height
+
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -23,6 +19,7 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
